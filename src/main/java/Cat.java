748fdd090e002d759e6b9a52f5f246a066247581
Frendzyfy <125387546+Frendzyfy@users.noreply.github.com>
2023-05-03 13:43:31 +0500

    Exactly

---
+++ src/main/java/Cat.java
@@ -0,0 +1,12 @@
+import java.io.Serializable;
+
+public class Cat {
+    private String message;
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
